d535fb8949d4378cd543137ca27573479dee4498
bisitelli <erik.bisi1@gmail.com>
2025-09-29 20:04:03 +0200

    layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700;800&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body>
         {children}
